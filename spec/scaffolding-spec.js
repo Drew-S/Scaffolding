17898da91ebdab12474e7b70d6a916a60f12b1db
Drew-S <Drew-S@users.noreply.github.com>
2017-02-25 15:08:38 -0700

    Trying specs. Local Atom tests fail, unofficial version of atom, arch.

--- spec/scaffolding-spec.js
+++ spec/scaffolding-spec.js
@@ -2,72 +2,34 @@
 
 import Scaffolding from '../lib/scaffolding';
 
-// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
-//
-// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
-// or `fdescribe`). Remove the `f` to unfocus the block.
-
 describe('Scaffolding', () => {
-  let workspaceElement, activationPromise;
+    let workspaceElement, scaffolding, activationPromise;
 
-  beforeEach(() => {
-    workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('scaffolding');
-  });
+    beforeEach(() => {
+        workspaceElement = atom.views.getView(atom.workspace);
+        jasmine.attachToDOM(workspaceElement);
 
-  describe('when the scaffolding:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.scaffolding')).not.toExist();
+        console.log(atom.packages.getActivePackage("scaffolding"));
 
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'scaffolding:toggle');
+        waitsForPromise(() => {
+            return atom.packages.activatePackage("scaffolding");
+        });
 
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.scaffolding')).toExist();
-
-        let scaffoldingElement = workspaceElement.querySelector('.scaffolding');
-        expect(scaffoldingElement).toExist();
-
-        let scaffoldingPanel = atom.workspace.panelForItem(scaffoldingElement);
-        expect(scaffoldingPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'scaffolding:toggle');
-        expect(scaffoldingPanel.isVisible()).toBe(false);
-      });
     });
 
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
+    describe('Running command scaffolding:toggle', () => {
+        it('Should not be visible yet', () => {
 
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
+            expect(workspaceElement.querySelector('.scaffolding')).not.toExist();
 
-      expect(workspaceElement.querySelector('.scaffolding')).not.toExist();
+        });
 
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'scaffolding:toggle');
+        it('Should be visable after toggling', () => {
+            atom.commands.dispatch(workspaceElement, "scaffolding:toggle");
 
-      waitsForPromise(() => {
-        return activationPromise;
-      });
+            expect(workspaceElement.querySelector('.scaffolding')).toExist();
+            expect(workspaceElement.querySelector('.scaffolding')).toBeVisible();
 
-      runs(() => {
-        // Now we can test for view visibility
-        let scaffoldingElement = workspaceElement.querySelector('.scaffolding');
-        expect(scaffoldingElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'scaffolding:toggle');
-        expect(scaffoldingElement).not.toBeVisible();
-      });
+        });
     });
-  });
 });
--- spec/scaffolding-view-spec.js
+++ spec/scaffolding-view-spec.js
@@ -4,37 +4,4 @@ import ScaffoldingView from '../lib/scaffolding-view';
 
 describe('ScaffoldingView', () => {
 
-  describe('when scaffolding:edit event is triggered', () => {
-    it("scaffolding.json should be open in editor", () => {
-      ScaffoldingView.edit(() => {
-        expect(editor.getPath().toContain('scaffolding.json'));
-      });
-    });
-  });
-
-  describe('Building a tree', () => {
-    it("Project should have new files", () => {
-      var obj = {
-        name: "test",
-        root: [
-            {
-            type: "dir",
-            name: "test",
-            root: [
-              {
-                type: "file",
-                name: "test.txt"
-              }
-            ]
-          }
-        ]
-      };
-      var root = atom.project.rootDirectories[0].path;
-      ScaffoldingView.buildScaffoldingStructure(obj, root, () => {
-        expect(atom.project.contains(root + "/test/test.txt")).toBe(true);
-      });
-
-    });
-
-  });
 });
